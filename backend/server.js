d556266c12b273715b6d24b01b80ec12c57c0741
TheDestor <52073245+TheDestor@users.noreply.github.com>
2025-04-26 22:18:44 +0800

    Fix again?

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import connectDB from "./config/mongodb.js";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/AuthRoutes.js";
@@ -10,11 +9,6 @@ import dashboardRouter from "./routes/DashboardRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 5050
 connectDB(); // Establish connection to the database as soon as the backend is run
-
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true
-}));
 
 app.use(express.json());
 app.use(cookieParser());
