705057c3804010d2f636ae20c3bce4b3ff98a5af
TheDestor <52073245+TheDestor@users.noreply.github.com>
2025-05-05 15:51:49 +0800

    Initial profile picture upload setup

--- frontend/src/pages/ProfileSettingpage.jsx
+++ frontend/src/pages/ProfileSettingpage.jsx
@@ -13,19 +13,19 @@ import ky from 'ky';
 const naCountryObject = { name: 'N/A', code: 'NA', flag: null };
 
 const countries = [
-    { name: 'Malaysia', code: 'MY', flag: 'https://flagcdn.com/w40/my.png' },
-    { name: 'Singapore', code: 'SG', flag: 'https://flagcdn.com/w40/sg.png' },
-    { name: 'Thailand', code: 'TH', flag: 'https://flagcdn.com/w40/th.png' },
-    { name: 'Indonesia', code: 'ID', flag: 'https://flagcdn.com/w40/id.png' },
-    { name: 'Philippines', code: 'PH', flag: 'https://flagcdn.com/w40/ph.png' },
-    { name: 'Vietnam', code: 'VN', flag: 'https://flagcdn.com/w40/vn.png' },
-    { name: 'Japan', code: 'JP', flag: 'https://flagcdn.com/w40/jp.png' },
-    { name: 'South Korea', code: 'KR', flag: 'https://flagcdn.com/w40/kr.png' },
-    { name: 'China', code: 'CN', flag: 'https://flagcdn.com/w40/cn.png' },
-    { name: 'India', code: 'IN', flag: 'https://flagcdn.com/w40/in.png' },
-    { name: 'United States', code: 'US', flag: 'https://flagcdn.com/w40/us.png' },
-    { name: 'United Kingdom', code: 'GB', flag: 'https://flagcdn.com/w40/gb.png' },
-  ];  
+  { name: 'Malaysia', code: 'MY', flag: 'https://flagcdn.com/w40/my.png' },
+  { name: 'Singapore', code: 'SG', flag: 'https://flagcdn.com/w40/sg.png' },
+  { name: 'Thailand', code: 'TH', flag: 'https://flagcdn.com/w40/th.png' },
+  { name: 'Indonesia', code: 'ID', flag: 'https://flagcdn.com/w40/id.png' },
+  { name: 'Philippines', code: 'PH', flag: 'https://flagcdn.com/w40/ph.png' },
+  { name: 'Vietnam', code: 'VN', flag: 'https://flagcdn.com/w40/vn.png' },
+  { name: 'Japan', code: 'JP', flag: 'https://flagcdn.com/w40/jp.png' },
+  { name: 'South Korea', code: 'KR', flag: 'https://flagcdn.com/w40/kr.png' },
+  { name: 'China', code: 'CN', flag: 'https://flagcdn.com/w40/cn.png' },
+  { name: 'India', code: 'IN', flag: 'https://flagcdn.com/w40/in.png' },
+  { name: 'United States', code: 'US', flag: 'https://flagcdn.com/w40/us.png' },
+  { name: 'United Kingdom', code: 'GB', flag: 'https://flagcdn.com/w40/gb.png' },
+];
 
 const naSelectOption = {
   value: 'N/A',
@@ -50,100 +50,122 @@ const countryOptions = [
   }))
 ];
 
-  const customStyles = {
-    control: (provided) => ({
-      ...provided,
-      minHeight: '36px',
-      borderRadius: '10px',
-      width: '70px',
-      padding: '0',
-    }),
-    valueContainer: (provided) => ({
-      ...provided,
-      justifyContent: 'center',
-      padding: '0',
-    }),
-    singleValue: (provided) => ({
-      ...provided,
-      margin: '0',
-    }),
-    indicatorsContainer: (provided) => ({
-      ...provided,
-      display: 'flex', // show dropdown arrow again
-      justifyContent: 'center',
-    }),
-    dropdownIndicator: (provided) => ({
-      ...provided,
-      padding: '4px',
-    }),
+const customStyles = {
+  control: (provided) => ({
+    ...provided,
+    minHeight: '36px',
+    borderRadius: '10px',
+    width: '70px',
+    padding: '0',
+  }),
+  valueContainer: (provided) => ({
+    ...provided,
+    justifyContent: 'center',
+    padding: '0',
+  }),
+  singleValue: (provided) => ({
+    ...provided,
+    margin: '0',
+  }),
+  indicatorsContainer: (provided) => ({
+    ...provided,
+    display: 'flex', // show dropdown arrow again
+    justifyContent: 'center',
+  }),
+  dropdownIndicator: (provided) => ({
+    ...provided,
+    padding: '4px',
+  }),
+};
+
+const AvatarModal = ({ onClose, onSave, accessToken }) => {
+  const [activeTab, setActiveTab] = useState('avatar');
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
+  const [selectedFile, setSelectedFIle] = useState(null);
+
+  const avatars = Array.from({ length: 15 }, (_, i) => `src/assets/avatar${i + 1}.png`);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedAvatar(imageUrl);
+    }
+    setSelectedFIle(file);
   };
- 
-  const AvatarModal = ({ onClose, onSave }) => {
-    const [activeTab, setActiveTab] = useState('avatar');
-    const [selectedAvatar, setSelectedAvatar] = useState(null);
-  
-    const avatars = Array.from({ length: 15 }, (_, i) => `src/assets/avatar${i + 1}.png`);
-  
-    return (
-      <div className="modal-overlay">
-        <div className="avatar-modal">
-          <div className="tab-header">
-            <button className={activeTab === 'avatar' ? 'active' : ''} onClick={() => setActiveTab('avatar')}>Avatar</button>
-            <button className={activeTab === 'camera' ? 'active' : ''} onClick={() => setActiveTab('camera')}>Camera</button>
-            <button className={activeTab === 'photo' ? 'active' : ''} onClick={() => setActiveTab('photo')}>Photo</button>
-          </div>
-  
-          <div className="tab-content">
-            {activeTab === 'avatar' && (
-              <div className="avatar-grid">
-                {avatars.map((src, i) => (
-                  <img
-                    key={i}
-                    src={src}
-                    alt={`avatar-${i}`}
-                    className={`avatar-img ${selectedAvatar === src ? 'selected' : ''}`}
-                    onClick={() => setSelectedAvatar(src)}
-                  />
-                ))}
-              </div>
-            )}
-            {activeTab === 'camera' && (
-              <p>Camera input goes here.</p>
-            )}
-            {activeTab === 'photo' && (
+
+  const handleSave = async () => {
+    const formData = new FormData();
+    formData.append('avatar', selectedFile)
+
+    try {
+      const response = await ky.post("/api/user/updateAvatar",
+        {
+          headers: { 'Authorization': `Bearer ${accessToken}` },
+          body: formData
+        }
+      );
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="avatar-modal">
+        <div className="tab-header">
+          <button className={activeTab === 'avatar' ? 'active' : ''} onClick={() => setActiveTab('avatar')}>Avatar</button>
+          <button className={activeTab === 'camera' ? 'active' : ''} onClick={() => setActiveTab('camera')}>Camera</button>
+          <button className={activeTab === 'photo' ? 'active' : ''} onClick={() => setActiveTab('photo')}>Photo</button>
+        </div>
+
+        <div className="tab-content">
+          {activeTab === 'avatar' && (
+            <div className="avatar-grid">
+              {avatars.map((src, i) => (
+                <img
+                  key={i}
+                  src={src}
+                  alt={`avatar-${i}`}
+                  className={`avatar-img ${selectedAvatar === src ? 'selected' : ''}`}
+                  onClick={() => setSelectedAvatar(src)}
+                />
+              ))}
+            </div>
+          )}
+          {activeTab === 'camera' && (
+            <p>Camera input goes here.</p>
+          )}
+          {activeTab === 'photo' && (
             <div className="upload-section">
-                <input
+              <input
                 type="file"
                 accept="image/png, image/jpeg, image/jpg"
-                onChange={(e) => {
-                    const file = e.target.files[0];
-                    if (file) {
-                    const imageUrl = URL.createObjectURL(file);
-                    setSelectedAvatar(imageUrl);
-                    }
-                }}
-                />
-                {selectedAvatar && (
+                onChange={handleFileChange}
+              />
+              {selectedAvatar && (
                 <img
-                    src={selectedAvatar}
-                    alt="Uploaded Preview"
-                    className="avatar-img selected"
-                    style={{ width: '100px', height: '100px', marginTop: '1rem' }}
+                  src={selectedAvatar}
+                  alt="Uploaded Preview"
+                  className="avatar-img selected"
+                  style={{ width: '100px', height: '100px', marginTop: '1rem' }}
                 />
-                )}
+              )}
             </div>
-            )}
-          </div>
-  
-          <div className="modal-actions">
-            <button onClick={onClose} className="cancel-btn2">Cancel</button>
-            <button onClick={() => onSave(selectedAvatar)} className="save-btn" disabled={!selectedAvatar}>Save</button>
-          </div>
+          )}
+        </div>
+
+        <div className="modal-actions">
+          <button onClick={onClose} className="cancel-btn2">Cancel</button>
+          <button onClick={handleSave} className="save-btn" disabled={!selectedAvatar}>Save</button>
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 const ProfileSettingsPage = () => {
   const [activeSection, setActiveSection] = useState('account');
   const [selectedCountry, setSelectedCountry] = useState(naCountryObject);
@@ -155,8 +177,8 @@ const ProfileSettingsPage = () => {
     email: '',
     phoneNumber: ''
   });
-  const {user, accessToken, updateUserContext } = useAuth();
-  
+  const { user, accessToken, updateUserContext } = useAuth();
+
   useEffect(() => {
     if (user) {
       setFormData({
@@ -166,7 +188,7 @@ const ProfileSettingsPage = () => {
         phoneNumber: user.phoneNumber || '',
         nationality: user.nationality || 'N/A'
       });
-      
+
       let matchedCountryObject = naCountryObject;
       if (user.nationality != 'N/A') {
         matchedCountryObject = countries.find(c => c.name === user.nationality) || naCountryObject;
@@ -187,7 +209,7 @@ const ProfileSettingsPage = () => {
 
   const handleCancelEdit = () => {
     setIsEditingProfile(false);
-    
+
     if (user) {
       setFormData({
         firstName: user.firstName || '',
@@ -242,10 +264,10 @@ const ProfileSettingsPage = () => {
     switch (activeSection) {
       case 'account':
 
-      const currentSelectValue = countryOptions.find(option => option.value === selectedCountry.name);
-        
+        const currentSelectValue = countryOptions.find(option => option.value === selectedCountry.name);
+
         return (
-            <div className="profile-section">
+          <div className="profile-section">
             <div className="section-header">
               <h2><MdPerson /> Account Preferences</h2>
               {!isEditingProfile && (
@@ -253,22 +275,22 @@ const ProfileSettingsPage = () => {
                   Edit
                 </button>
               )}
-            </div>          
-            <div className="profile-row">
-            <div className="profile-picture">
-                <img src={UserImage} alt="Profile" />
             </div>
-            <div className="picture-buttons">
+            <div className="profile-row">
+              <div className="profile-picture">
+                <img src={UserImage} alt="Profile" />
+              </div>
+              <div className="picture-buttons">
                 <button className="change-btn" onClick={() => setShowPhotoModal(true)}>
-                    <MdOutlinePhoto /> Change
+                  <MdOutlinePhoto /> Change
                 </button>
 
                 <button className="remove-btn"><MdDelete /> Remove</button>
-            </div>
-            <div className="field-group nationality-group">
+              </div>
+              <div className="field-group nationality-group">
                 <label>Nationality</label>
                 <div className="nationality-row">
-                <Select
+                  <Select
                     options={countryOptions}
                     value={currentSelectValue}
                     onChange={(selectedOption) => {
@@ -283,10 +305,10 @@ const ProfileSettingsPage = () => {
                     styles={customStyles}
                     isSearchable={false}
                     isDisabled={!isEditingProfile}
-                    />
-                <input type="text" value={selectedCountry.name} readOnly className="nationality-input" />
+                  />
+                  <input type="text" value={selectedCountry.name} readOnly className="nationality-input" />
                 </div>
-            </div>
+              </div>
             </div>
             <div className="profile-fields">
               <div className="field-group">
@@ -296,7 +318,7 @@ const ProfileSettingsPage = () => {
               <div className="field-group">
                 <label>Last name</label>
                 <input id="lastName" name="lastName" type="text" value={formData.lastName} onChange={handleInputChange} readOnly={!isEditingProfile}
-                className="input-field"/>
+                  className="input-field" />
               </div>
               <div className="field-group email-group">
                 <label>Email</label>
@@ -309,31 +331,31 @@ const ProfileSettingsPage = () => {
                     readOnly={true}
                     disabled={true}
                     className="input-readonly input-field"
-                    />
+                  />
                 </div>
-                </div>
+              </div>
 
-                <div className="field-group phone-group">
-                  <label>Phone number</label>
-                  <div className="input-wrapper">
-                    <input
-                      id="phoneNumber"
-                      name="phoneNumber"
-                      type="tel"
-                      value={formData.phoneNumber}
-                      readOnly={true}
-                      disabled={true}
-                      className="input-readonly input-field"
-                    />
-                  </div>
+              <div className="field-group phone-group">
+                <label>Phone number</label>
+                <div className="input-wrapper">
+                  <input
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    type="tel"
+                    value={formData.phoneNumber}
+                    readOnly={true}
+                    disabled={true}
+                    className="input-readonly input-field"
+                  />
                 </div>
+              </div>
             </div>
 
             {isEditingProfile && (
-            <div className="profile-buttons">
+              <div className="profile-buttons">
                 <button className="cancel-btn6" onClick={handleCancelEdit}>Cancel</button>
                 <button className="update-btn" onClick={handleUpdateProfile}>Update</button>
-            </div>
+              </div>
             )}
           </div>
         );
@@ -365,14 +387,15 @@ const ProfileSettingsPage = () => {
       </div>
       {showPhotoModal && (
         <AvatarModal
-            onClose={() => setShowPhotoModal(false)}
-            onSave={(avatar) => {
+          onClose={() => setShowPhotoModal(false)}
+          onSave={(avatar) => {
             console.log("Avatar selected:", avatar);
             setShowPhotoModal(false);
             // You can set profile picture state here
-            }}
+          }}
+          accessToken={accessToken}
         />
-        )}
+      )}
     </div>
   );
 };
